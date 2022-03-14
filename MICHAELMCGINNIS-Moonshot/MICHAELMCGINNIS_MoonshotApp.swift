c5870ba77fe5273e058ddc38b773324b77975923
Rukosa <mwmcginnis490@gmail.com>
2022-03-14 14:38:00 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-Moonshot/MICHAELMCGINNIS_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_MoonshotApp.swift
+//  MICHAELMCGINNIS-Moonshot
+//
+//  Created by Michael Mcginnis on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
